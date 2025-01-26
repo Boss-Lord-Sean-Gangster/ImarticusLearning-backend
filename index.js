383653c0c4681d004d22a378966e3c0ccf0e2d63
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2025-01-26 10:02:16 +0530

    Changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(express.json());  // For parsing JSON bodies
 app.use(cors({
-  origin: ['http://localhost:5173','https://imarticus-learning-assignment.vercel.app/']
+  origin: 'http://localhost:5173'
 }));  // Enable CORS (if needed for frontend requests)
 
 // Import routes
